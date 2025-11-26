02c3e29d57891df377ae4c1ce64c86b99ab42e8f
shahzaib <shahzebraheel61@gmail.com>
2025-11-27 01:28:07 +0200

    index.js updated

--- server_copy/index.js
+++ server_copy/index.js
@@ -25,7 +25,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Handle preflight OPTIONS requests for all routes
-app.options('*', cors(corsOptions));
+app.options('/*', cors(corsOptions));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
